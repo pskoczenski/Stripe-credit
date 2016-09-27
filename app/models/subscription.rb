cbfcf77bcadf086cd6f515ca9364c4cc46d90222
pskoczenski <pskoczenski@gmail.com>
2016-09-26 21:38:18 -0400

    finish form and add js

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,5 @@
 class Subscription < ApplicationRecord
+	attr_accessor :full_name
+	attr_accessor :email
+	attr_accessor :telephone
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'subscriptions#new'
 
-  get 'subscriptions/create'
+  resource 'subscriptions'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
